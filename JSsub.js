755540aded3d9e3280e1cb8403b9b24a4b825ae9
harvey <heweimr@gmail.com>
2017-04-21 18:27:11 +0800

    成功接收文件代码并运行

--- JSsub.js
+++ JSsub.js
@@ -1,6 +1,8 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://127.0.0.1');
  
+var x=11,y=22;
+
 client.on('connect', function () {
   //订阅presence主题
   client.subscribe('presence');
@@ -13,8 +15,11 @@ client.on('connect', function () {
  
 client.on('message', function (topic, message) {
   //收到的消息是一个Buffer
-  if(topic === 'toDevice')
-  	console.log('-- Device Sub:'+ eval(message.toString()));
+  if(topic === 'toDevice'){
+  	console.log('\n-- Device Sub:');
+  	eval(message.toString());
+  }
+  	
   else 
   	console.log('## Device Sub:'+ message.toString());
   // client.end();
