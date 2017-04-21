18fecded7c4fdcd9b5d2e73e45126c04ad7e7340
harvey <heweimr@gmail.com>
2017-04-21 19:20:23 +0800

    发送设备信息

--- JSpub.js
+++ JSpub.js
@@ -1,15 +1,21 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://127.0.0.1');
+
+var device_json={"secretkey":"93cf82461c324858961b6cc70fc5033d","deviceprofile":{"devicename":"hhhh","IP":"1.1.1.1","location":"23","tags":"33","latitude":"33","longitude":"22","sensors":[{"name":"22","sid":"22","channels":[{"name":"22","type":"22","unit":"22","samplingperiod":"22"}]},{"name":"22","sid":"22","channels":[{"name":"22","type":"22","unit":"22","samplingperiod":"22"}]}],"actuators":[{"name":"22name"}]}};
+
+
  
 client.on('connect', function () {
   //向presence主题发布消息
   setInterval(function(){
   	client.publish('presence', 'Hello mqtt');
-  	console.log('## Device pub: Hello mqtt')
+  	console.log('\n## Device pub: Hello mqtt')
   },3000);
   
-  // setInterval(function(){
-  // 	client.publish('short', '------');
-  // },1000);
+  setInterval(function(){
+  	device_json['deviceprofile']['devicename']='devicename'+new Date().getTime();
+  	client.publish('ICS', JSON.stringify(device_json));
+  	console.log('\n##-- Device pub: '+ JSON.stringify(device_json))
+  },1000);
 });
   
