0df07314137c17ce8c2ab256188b61c15e48219b
harvey <heweimr@gmail.com>
2017-04-26 21:57:03 +0800

    -

--- JSsub.js
+++ JSsub.js
@@ -1,14 +1,15 @@
 var mqtt    = require('mqtt');
 var client  = mqtt.connect('mqtt://127.0.0.1');
+// var client = mqtt.connect('mqtt://192.168.199.170'); //树莓派ip
  
 var x=11,y=22;
 
 client.on('connect', function () {
-
   client.subscribe('Environment');
   client.subscribe('RaspberryPiInfo');
   client.subscribe('toDevice');
- 
+
+  client.subscribe('Temperature');
 });
  
 client.on('message', function (topic, message) {
@@ -16,9 +17,20 @@ client.on('message', function (topic, message) {
   if(topic === 'toDevice'){
   	console.log('\n-- Device Sub:');
   	eval(message.toString());
-  }
-  	
+  }	
   else 
   	console.log('## Device Sub:'+ message.toString());
   // client.end();
 });
+
+
+// -----------------------------------------------------
+// var client2 = mqtt.connect('mqtt://192.168.199.170');
+// client2.on('connect', function () {
+//   client2.subscribe('Environment');
+// });
+ 
+// client2.on('message', function (topic, message) {
+//   	console.log('～～ Device Sub:'+ message.toString());
+//   // client.end();
+// });
