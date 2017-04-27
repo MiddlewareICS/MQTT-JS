02260319ef5415ad7aa796b0e19151d7d3a1a82f
harvey <heweimr@gmail.com>
2017-04-27 18:54:53 +0800

    实现mac和树莓派之间的订阅发布

--- JSsub.js
+++ JSsub.js
@@ -5,30 +5,24 @@ var PiUrl = 'mqtt://192.168.199.170';//树莓派ip
 var heweiUrl = 'mqtt://192.168.199.231';//hewei ip
 
 //Sclient :server端为代理时，Dclient ：device端为代理时
+
+// --------------------- Server Sub --------------------------------
 var Sclient = mqtt.connect(heweiUrl); 
-
-
 var x=11,y=22;
-
 Sclient.on('connect', function () {
-  Sclient.subscribe('Environment');
-  Sclient.subscribe('RaspberryPiInfo');
   Sclient.subscribe('toDevice');
 });
- 
 Sclient.on('message', function (topic, message) {
-  //收到的消息是一个Buffer
   if(topic === 'toDevice'){
   	console.log('\n-- Device Sub:');
   	eval(message.toString());
   }	
   else 
-  	console.log('## Device Sub:'+ message.toString());
-  // client.end();
+  	console.log('\n-- Device Sub:'+ message.toString());
 });
 
 
-// --------------------- DeviceLocal --------------------------------
+// --------------------- Device Sub --------------------------------
 var Dclient = mqtt.connect(localUrl);
 Dclient.on('connect', function () {
   Dclient.subscribe('Environment');
@@ -36,7 +30,7 @@ Dclient.on('connect', function () {
 });
  
 Dclient.on('message', function (topic, message) {
-  	console.log('～～ Device Sub:'+ message.toString());
+  	console.log('## Device Sub:'+ message.toString());
     Sclient.publish(topic,message.toString());           //发送给服务器
   // client.end();
 });
